f1814ff4f8f06e554847fffd8961ad53349cf398
StraetenMax <maxstraeten@yahoo.com>
2017-03-27 09:56:28 +0200

    installation d'un deuxième constructeur et de la classe main

---
+++ src/PgmPal.java
@@ -0,0 +1,9 @@
+
+public class PgmPal {
+
+	public static void main(String[] args) {
+		Voiture voiture = new Voiture();
+		Voiture voiture2 = new Voiture(4, "Bleu", "Essence", 170);
+	}
+
+}
--- src/Voiture.java
+++ src/Voiture.java
@@ -6,12 +6,21 @@ public class Voiture {
 	String energie;
 	int vitesseMax;
 
-	//constructeur
+	//constructeur vide
 	public Voiture(){
 		System.out.println("Creation d'une voiture !");
 		nbOccupants = 0;
 		couleur = "Inconnu";
 		energie = "Inconnu";
 		vitesseMax = 0;
+	}
+	
+	//constructeur initialisé
+	public Voiture(int cNbOccupants, String cCouleur, String cEnergie, int cVitesseMax){
+	System.out.println("Création d'une voiture initialisée !");
+	nbOccupants = cNbOccupants;
+	couleur = cCouleur;
+	energie = cEnergie;
+	vitesseMax = cVitesseMax;
 	}
 }
