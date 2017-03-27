b3fa726fb32a3e39868d653027d24bd0bf97400a
StraetenMax <maxstraeten@yahoo.com>
2017-03-27 10:15:39 +0200

    variables d'instance

--- src/PgmPal.java
+++ src/PgmPal.java
@@ -4,6 +4,13 @@ public class PgmPal {
 	public static void main(String[] args) {
 		Voiture voiture = new Voiture();
 		Voiture voiture2 = new Voiture(4, "Bleu", "Essence", 170);
+		Voiture voiture3 = new Voiture();
+		Voiture voiture4 = new Voiture(6, "Rouge", "Diesel", 200);
+		System.out.println(voiture4.couleur);
+		System.out.println(voiture2.energie);
+		System.out.println(voiture4.nbOccupants);
+		System.out.println(voiture4.vitesseMax);
 	}
 
 }
+
